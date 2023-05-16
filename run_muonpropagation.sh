41efdb442c6e8a9bf2a26466063adc8a71e9c49c
BraydenDillon <133713194+BraydenDillon@users.noreply.github.com>
2023-05-16 14:43:24 -0400

    Update run_muonpropagation.sh
    Changing Path

--- run_muonpropagation.sh
+++ run_muonpropagation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-envs=/home/users/twagiray/pone_offline/env-shell_Container.sh
-pyfile=/data/p-one/sim/muongun/sim0005/src/GenerateEvents_MuonGun.py
-gcd=/data/p-one/sim/muongun/sim0005/gcdfile/PONE_10String_7Cluster_standard.i3.gz
-outfile=/data/p-one/sim/muongun/sim0005/muonprop
-for i in $(seq 0 1 999)
+envs=/home/users/bdillon/pone_offline/env-shell_Container.sh
+pyfile=/home/users/bdillon/P-ONE/sim0001/src/GenerateEvents_MuonGun.py
+gcd=/home/users/bdillon/P-ONE/sim0001/gcdfile/PONE_10String_7Cluster_standard.i3.gz
+outfile=/home/users/bdillon/P-ONE/sim0001/muonprop
+for i in $(seq 0 1 10)
 do
-	./submit_job.sh bash $envs python $pyfile -g $gcd -o $outfile/GenerateSingleMuons_$i.i3.gz -n 500 -r $i
+	./submit_job.sh bash $envs python $pyfile -g $gcd -o $outfile/GenerateSingleMuons_$i.i3.gz -n 10 -r $i
 done
