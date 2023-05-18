1abb0d0ff4b4b364cf3bd4ffeb7f79f60e3d8004
BraydenDillon <133713194+BraydenDillon@users.noreply.github.com>
2023-05-18 12:12:50 -0400

    Update run_propogatePhotons.sh

--- run_propogatePhotons.sh
+++ run_propogatePhotons.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-envs=/home/users/twagiray/pone_offline/env-shell_Container.sh
-infile=/data/p-one/sim/muongun/sim0005/muonprop/*.i3.gz
-pyfile=/data/p-one/sim/muongun/sim0005/src/PropogatePhotons.py
-gcd=/data/p-one/sim/muongun/sim0005/gcdfile/PONE_10String_7Cluster_standard.i3.gz
-outfile=/data/p-one/sim/muongun/sim0005/photonprop
+envs=/home/users/bdillon/pone_offline/env-shell_Container.sh
+infile= /home/users/bdillon/P-ONE/sim0001/muonprop/*.i3.gz
+pyfile= /home/users/bdillon/P-ONE/sim0001/src/PropogatePhotons.py
+gcd= /home/users/bdillon/P-ONE/sim0001/gcdfile/PONE_10String_7Cluster_standard.i3.gz
+outfile=/home/users/bdillon/P-ONE/sim0001/photonprop
 for file in $infile
 do
     ./submit_job.sh bash $envs python $pyfile -i $file -g $gcd -o $outfile/$(basename $file .i3.gz)_photonprop.i3.gz
