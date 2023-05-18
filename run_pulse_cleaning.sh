02a6d1e3bdd0b0c12dd49d22f792a4ad33a24625
BraydenDillon <133713194+BraydenDillon@users.noreply.github.com>
2023-05-18 12:16:55 -0400

    Update run_pulse_cleaning.sh

--- run_pulse_cleaning.sh
+++ run_pulse_cleaning.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-envs=/home/users/twagiray/pone_offline/env-shell_Container.sh
-infile=/data/p-one/sim/muongun/sim0005/daqsim/GenerateSingleMuons*_daqSim_noise_ON.i3.gz
-pyfile=/data/p-one/sim/muongun/sim0005/src/Pulse_cleaning.py
-gcd=/data/p-one/sim/muongun/sim0005/gcdfile/PONE_10String_7Cluster_standard.i3.gz
-outfile=/data/p-one/sim/muongun/sim0005/linefit
+envs=/home/users/bdillon/pone_offline/env-shell_Container.sh
+infile=/home/users/bdillon/P-ONE/sim0001/daqsim/GenerateSingleMuons*_daqSim_noise_ON.i3.gz
+pyfile=/home/users/bdillon/P-ONE/sim0001/src/Pulse_cleaning.py
+gcd=/home/users/bdillon/P-ONE/sim0001/gcdfile/PONE_10String_7Cluster_standard.i3.gz
+outfile=/home/users/bdillon/P-ONE/sim0001/linefit
 for file in $infile
 do
     ./submit_job.sh bash $envs python $pyfile -i $file -g $gcd -o $outfile/$(basename $file .i3.gz)_clean_linefit.i3.gz
