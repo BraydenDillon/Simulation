a493739ce03031e0119c9d8c2430ce9718d00c23
BraydenDillon <133713194+BraydenDillon@users.noreply.github.com>
2023-05-22 14:34:11 -0400

    Update run_pulse_cleaning.sh

--- run_pulse_cleaning.sh
+++ run_pulse_cleaning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 envs=/home/users/bdillon/pone_offline/env-shell_Container.sh
 infile=/home/users/bdillon/P-ONE/sim0001/daqsim/GenerateSingleMuons*_daqSim_noise_ON.i3.gz
-pyfile=/home/users/bdillon/P-ONE/sim0001/src/Pulse_cleaning.py
+pyfile=/home/users/bdillon/P-ONE/sim0001/src_sim/Pulse_cleaning.py
 gcd=/home/users/bdillon/P-ONE/sim0001/gcdfile/PONE_10String_7Cluster_standard.i3.gz
 outfile=/home/users/bdillon/P-ONE/sim0001/linefit
 for file in $infile
