fa3022bda3ec7e9d0ec1ac5710bef03a8dd6cfc6
BraydenDillon <133713194+BraydenDillon@users.noreply.github.com>
2023-05-22 14:33:48 -0400

    Update run_muonpropagation.sh

--- run_muonpropagation.sh
+++ run_muonpropagation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 envs=/home/users/bdillon/pone_offline/env-shell_Container.sh
-pyfile=/home/users/bdillon/P-ONE/sim0001/src/GenerateEvents_MuonGun.py
+pyfile=/home/users/bdillon/P-ONE/sim0001/src_sim/GenerateEvents_MuonGun.py
 gcd=/home/users/bdillon/P-ONE/sim0001/gcdfile/PONE_10String_7Cluster_standard.i3.gz
 outfile=/home/users/bdillon/P-ONE/sim0001/muonprop
 for i in $(seq 0 1 10)
