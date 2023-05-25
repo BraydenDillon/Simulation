2f64fa6a8ef302967ee7777428e113f3fb32ea0b
BraydenDillon <133713194+BraydenDillon@users.noreply.github.com>
2023-05-25 10:32:04 -0400

    Update run_muonpropagation.sh
    Scaling up simulation volume to 100,000

--- run_muonpropagation.sh
+++ run_muonpropagation.sh
@@ -3,7 +3,7 @@ envs=/home/users/bdillon/pone_offline/env-shell_Container.sh
 pyfile=/home/users/bdillon/P-ONE/sim0001/src_sim/GenerateEvents_MuonGun.py
 gcd=/home/users/bdillon/P-ONE/sim0001/gcdfile/PONE_10String_7Cluster_standard.i3.gz
 outfile=/home/users/bdillon/P-ONE/sim0001/muonprop
-for i in $(seq 0 1 10)
+for i in $(seq 0 1 999)
 do
-	./submit_job.sh bash $envs python $pyfile -g $gcd -o $outfile/GenerateSingleMuons_$i.i3.gz -n 10 -r $i
+	./submit_job.sh bash $envs python $pyfile -g $gcd -o $outfile/GenerateSingleMuons_$i.i3.gz -n 100 -r $i
 done
